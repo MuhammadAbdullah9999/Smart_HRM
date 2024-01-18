cc5ef6742e8ded8d65538cdbe2927448521d7ca2
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-01-19 01:08:14 +0500

    login auth

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,17 +1,15 @@
 import * as React from "react";
 import LoginImage from "../images/log1.jpg";
 
-
 function Login(props) {
   return (
-    <div className="flex h-screen">
+    <div className="flex flex-col lg:flex-row h-screen">
       {/* Left side */}
-      <div className="flex w-1/2 border-black-100 border-5">
-        <div className="flex flex-col gap-8 p-8 bg-bg-color text-white w-full">
-       
+      <div className="lg:w-1/2 border-black-100 border-5 order-2 lg:order-1">
+        <div className="flex flex-col justify-center gap-8 p-8 bg-bg-color text-white w-full h-full">
           <div className="flex flex-col gap-5">
-            <h2 className="text-5xl font-bold">Login</h2>
-            <p className="text-2xl">Enter your Credentials to Login</p>
+            <h2 className="text-5xl lg:text-5xl font-bold">Login</h2>
+            <p className="text-2xl lg:text-2xl">Enter your Credentials to Login</p>
           </div>
           <div className="flex flex-col gap-4">
             <div className="text-black">
@@ -29,14 +27,14 @@ function Login(props) {
               />
             </div>
           </div>
-          <button className="bg-blue-500 text-white p-2 rounded-lg w-full">
+          <button className="bg-blue-800 text-white p-2 rounded-lg w-full active:bg-white active:text-blue-800">
             Login
           </button>
         </div>
       </div>
 
       {/* Right side */}
-      <div className="w-1/2 border-gray-100 border-5">
+      <div className="lg:w-1/2 border-gray-100 border-5 order-1 lg:order-2">
         <img src={LoginImage} alt="login" className="w-full h-full object-cover" />
       </div>
     </div>
