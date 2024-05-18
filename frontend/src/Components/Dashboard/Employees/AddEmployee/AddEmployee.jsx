b0b6f6f29c1df694546f66921f53f6e78f050cd9
MuhammadAbdullah9999 <hafizzabdullah999@gmail.com>
2024-05-19 00:39:27 +0500

    Add employee correction

--- frontend/src/Components/Dashboard/Employees/AddEmployee/AddEmployee.jsx
+++ frontend/src/Components/Dashboard/Employees/AddEmployee/AddEmployee.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import validator from "validator";
@@ -17,15 +16,18 @@ const AddEmployee = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const employeeData = useSelector((state) => state.EmployeeData.EmployeeData);
-  console.log(employeeData)
   const [departments, setDepartments] = useState([]);
   const [formData, setFormData] = useState({
-    organizationId: employeeData.userType==='business_owner'?employeeData.user._id: employeeData.user.organizationId,
+    organizationId:
+      employeeData.userType === "business_owner"
+        ? employeeData.user._id
+        : employeeData.user.organizationId,
     hrEmail: employeeData.user.email,
     employeeId: "021",
-    name:"usama",
+    name: "usama",
     position: "software engineer",
-    department: employeeData.userType === "business_owner" ? "human resource" : "",
+    department:
+      employeeData.userType === "business_owner" ? "human resource" : "",
     dateOfBirth: "12-04-1977",
     contact: "0321111222333",
     email: "usama@devsinc.com",
@@ -43,7 +45,9 @@ const AddEmployee = () => {
     const getDepartments = async () => {
       try {
         setLoading(true);
-        const response = await axios.get(`http://localhost:5000/Departments/GetDepartments/${employeeData.user.organizationId}`);
+        const response = await axios.get(
+          `http://localhost:5000/Departments/GetDepartments/${employeeData.user.organizationId}`
+        );
         setDepartments(response.data.departments);
         setLoading(false);
       } catch (error) {
@@ -75,20 +79,19 @@ const AddEmployee = () => {
   const handleImageChange = (e) => {
     const selectedImage = e.target.files[0];
     setImage(selectedImage);
-  
+
     setErrors({
       ...errors,
       image: "",
     });
   };
-  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     // Perform form validation
     let hasErrors = false;
-  
+
     if (!validator.isEmail(formData.email)) {
       setErrors((prevErrors) => ({
         ...prevErrors,
@@ -96,7 +99,7 @@ const AddEmployee = () => {
       }));
       hasErrors = true;
     }
-  
+
     // Check for empty fields
     Object.entries(formData).forEach(([field, value]) => {
       if (value === "" && field !== "allowances") {
@@ -107,7 +110,7 @@ const AddEmployee = () => {
         hasErrors = true;
       }
     });
-  
+
     // Validate numeric input for salary and allowances
     if (formData.salary && !validator.isNumeric(formData.salary)) {
       setErrors((prevErrors) => ({
@@ -116,64 +119,63 @@ const AddEmployee = () => {
       }));
       hasErrors = true;
     }
-  
-    // if (formData.allowances && !validator.isNumeric(formData.allowances)) {
-    //   setErrors((prevErrors) => ({
-    //     ...prevErrors,
-    //     allowances: "Please enter a valid numeric value",
-    //   }));
-    //   hasErrors = true;
-    // }
-    // if (!image) {
-    //   setErrors((prevErrors) => ({
-    //     ...prevErrors,
-    //     image: "Please select an image",
-    //   }));
-    //   hasErrors = true;
-    // }
-  
+
+    if (employeeData.userType !== "business_owner" && formData.department === "") {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        department: "Please select a department",
+      }));
+      hasErrors = true;
+    }
+
+    if (!image) {
+      setErrors((prevErrors) => ({
+        ...prevErrors,
+        image: "Please select an image",
+      }));
+      hasErrors = true;
+    }
+
     if (hasErrors) {
       return;
     }
-  
+
     try {
       setApiError("");
       setLoading(true);
-  
-      // const formDataWithImage = new FormData();
-      // formDataWithImage.append("image", image); // Append the image correctly
-      
-      
-      // Object.entries(formData).forEach(([key, value]) => {
-      //   formDataWithImage.append(key, value);
-      // });
 
-      console.log("FormData with Image:", formData);
+      const formDataWithImage = new FormData();
+      formDataWithImage.append("image", image); // Append the image correctly
+
+      Object.entries(formData).forEach(([key, value]) => {
+        formDataWithImage.append(key, value);
+      });
+
+      console.log("FormData with Image:", formDataWithImage);
       let apiUrl = "http://localhost:5000/AddEmployee";
-  
+
       if (employeeData.userType === "business_owner") {
         apiUrl = "http://localhost:5000/AddHR"; // Use AddHR endpoint for business owners
       }
-  
-     const response = await axios.post(apiUrl, formData, {
-  // headers: {
-  //   "Content-Type": "multipart/form-data",
-  // },
-});
+
+      const response = await axios.post(apiUrl, formDataWithImage, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
       if (response.data) {
-        console.log(response.data)
+        console.log(response.data);
         setLoading(false);
         dispatch(setEmployeeData(response.data.data));
         setApiError("");
-        if(employeeData.userType==='business_owner'){
-          navigate('/CEO/dashboard/employees')
-        }
-        else{
+        if (employeeData.userType === "business_owner") {
+          navigate("/CEO/dashboard/employees");
+        } else {
           navigate("/HR/dashboard/employees");
         }
       }
-  
+
       setFormData({
         employeeId: "",
         name: "",
@@ -187,13 +189,12 @@ const AddEmployee = () => {
         allowances: "",
       });
     } catch (error) {
-      // Handle error
       setLoading(false);
       console.error(error);
       setApiError(error.response.data.error);
     }
   };
-  
+
   return (
     <div
       className={`flex gap-12 ${
@@ -207,31 +208,25 @@ const AddEmployee = () => {
       )}
       <div>
         <div className="fixed">
-        {employeeData && employeeData.userType === "business_owner" ? (
-        <CeoSidebar />
-      ) : (
-        <Sidebar />
-      )}
+          {employeeData && employeeData.userType === "business_owner" ? (
+            <CeoSidebar />
+          ) : (
+            <Sidebar />
+          )}
         </div>
       </div>
       <div className="w-full mt-4 ml-72 mr-8">
-        <DashboardOverview pageName={employeeData && employeeData.userType === "business_owner" ?"Add HR":'Add Employee'}></DashboardOverview>
+        <DashboardOverview
+          pageName={
+            employeeData && employeeData.userType === "business_owner"
+              ? "Add HR"
+              : "Add Employee"
+          }
+        ></DashboardOverview>
 
         <div className="w-full px-12 mr-4 p-6 pt-16 rounded-xl shadow-md grid grid-cols-1 md:grid-cols-1 gap-4 mt-4 bg-slate-200">
           <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-2 gap-4">
-              {/* <InputField
-                label="Employee ID"
-                type="text"
-                id="employeeId"
-                name="employeeId"
-                autoComplete="off"
-                value={formData.employeeId}
-                error={errors.employeeId}
-                onChange={handleInputChange}
-                focusColor="black"
-                top="6"
-              /> */}
               <InputField
                 label="Name"
                 type="text"
@@ -256,53 +251,42 @@ const AddEmployee = () => {
                 focusColor="black"
                 top="6"
               />
-              {/* <InputField
-                label="Department"
-                type="text"
-                id="department"
-                name="department"
-                autoComplete="off"
-                value={formData.department}
-                error={errors.department}
-                onChange={handleInputChange}
-                focusColor="black"
-                top="6"
-              /> */}
               <div className="mb-4 relative">
-            <label
-              htmlFor="numberOfHrs"
-              className={`absolute transition-all duration-300 ${
-                formData.numberOfHrs ? "text-sm text-white -top-6 left-1" : "top-2 left-3 text-gray-500 text-xs md:text-sm lg:text-sm"
-              }`}
-            >
-              {/* {formData.department ? "Select Departments" : ""} */}
-            </label>
-            {employeeData.userType === "business_owner" ? (
-  <div className="p-2 border rounded w-full bg-gray-100">
-    <p className="text-black">Human Resource</p>
-  </div>
-) : (
-  <select
-    id="department"
-    name="department"
-    className={`p-2 border rounded w-full outline-none ${
-      formData.department ? "text-black" : "text-gray-500"
-    } bg-white`}
-    autoComplete="off"
-    value={formData.department}
-    onChange={handleInputChange}
-  >
-    <option value="">Select Departments</option>
-    {departments.map((option, index) => (
-      <option key={index} value={option}>
-        {option}
-      </option>
-    ))}
-  </select>
-)}
-
-            
-          </div>
+                <label
+                  htmlFor="department"
+                  className={`absolute transition-all duration-300 ${
+                    formData.department
+                      ? "text-sm text-white -top-6 left-1"
+                      : "top-2 left-3 text-gray-500 text-xs md:text-sm lg:text-sm"
+                  }`}
+                ></label>
+                {employeeData.userType === "business_owner" ? (
+                  <div className="p-2 border rounded w-full bg-gray-100">
+                    <p className="text-black">Human Resource</p>
+                  </div>
+                ) : (
+                  <select
+                    id="department"
+                    name="department"
+                    className={`p-2 border rounded w-full outline-none ${
+                      formData.department ? "text-black" : "text-gray-500"
+                    } bg-white`}
+                    autoComplete="off"
+                    value={formData.department}
+                    onChange={handleInputChange}
+                  >
+                    <option value="">Select Department</option>
+                    {departments.map((option, index) => (
+                      <option key={index} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </select>
+                )}
+                {errors.department && (
+                  <p className="text-red-800 font-bold mt-1">{errors.department}</p>
+                )}
+              </div>
               <InputField
                 label="Date of Birth"
                 type="text"
@@ -363,19 +347,7 @@ const AddEmployee = () => {
                 focusColor="black"
                 top="6"
               />
-              {/* <InputField
-                label="Allowances"
-                type="text"
-                id="allowances"
-                name="allowances"
-                autoComplete="off"
-                value={formData.allowances}
-                error={errors.allowances}
-                onChange={handleInputChange}
-                focusColor="black"
-                top="6"
-              /> */}
-              {/* <div className="">
+              <div className="">
                 <label className="block text-sm font-medium text-gray-700">
                   Image
                 </label>
@@ -388,7 +360,7 @@ const AddEmployee = () => {
                 {errors.image && (
                   <p className="text-red-800 font-bold mt-1">{errors.image}</p>
                 )}
-              </div> */}
+              </div>
             </div>
             <div className="">
               {apiError && (
@@ -396,10 +368,14 @@ const AddEmployee = () => {
               )}
               <button
                 type="submit"
-                className="flex justify-center m-auto sm:col-span-2 bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer lg:mt-6 md:mt-4"
+                className="flex justify-center m-auto hover:bg-blue-500 active:bg-slate-100 active:text-blue-600 sm:col-span-2 bg-blue-600 text-white px-4 py-2 rounded-lg cursor-pointer lg:mt-6 md:mt-4"
               >
-                <Add className="mr-1"></Add>
-                <p className="text-1xl font-bold">{employeeData.userType==='business_owner'?'Add HR':'Add Employee'}</p>
+                <Add className="mr-1" />
+                <p className="text-1xl font-bold">
+                  {employeeData.userType === "business_owner"
+                    ? "Add HR"
+                    : "Add Employee"}
+                </p>
               </button>
             </div>
           </form>
